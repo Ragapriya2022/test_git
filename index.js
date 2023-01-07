86972ceb3e4eab1947b385563d2acd33237b79b4
Ragapriya <learnndwork@gmail.com>
2023-01-07 14:08:26 +0000

    stage and commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function addtwonumbers(num1, num2){
+    return num1+num2;
+
+}
+console.log(addtwonumbers(1,2));
+
+
+const calculator ={};
+calculator,add = addtwonumbers;
+module.exports = calculator;
